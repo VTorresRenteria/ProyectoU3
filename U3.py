c33a820b4f6a301c33c45aca38d2b043feb768e6
EmilianoZNape <emiliano.sa.ph@gmail.com>
2025-11-19 21:42:37 -0600

    Final

--- U3.py
+++ U3.py
@@ -82,10 +82,47 @@ while True:
         print(f"\n1. Instanciar\n2. Desplegar\n3. Salir")
         opcion = int(input("\nIngresa la opción: "))
         if opcion == 1:
-            lavadora = Lavadora("8MWTW2224WJM", "Whirlpool", "8MWTW2224WJM", 13999, 22, 15 ,12)
-            refrigerador = Refrigerador("RF22A4010S9/EM","Samsung","RF22A4010S9/EM",30000,3,0.623,22)
-            microondas = Microondas ("MH1596DIR","LG","NeoChef",4700,1200,1350,"54x32.2x43.3")
-            print("\nSe instanciaron los objetos")
+            opcion_insta = int(input(print(f"\n1. Lavadora\n2. Refrigerador\n3. microondas")))
+
+
+            if opcion_insta == 1:
+                id_lavadora = input("ingrese la id de la lavadora")
+                marca_lavadora = input("ingrese la marca de lava")
+                modelo_lavadora = input("ingrese el modelo de lavadora")
+                prec_lavadora = float(input("ingrese el precio de la lavadora"))
+                capacidad_carga = int(input("ingrese la capacidad de carga de la lavadora en kg"))
+                consumo_agua = int(input("ingrese el consumo de agua de la lavadora en litros"))
+                ciclos_lavado = int(input("ingrese el numero de ciclos de lavado de la lavadora"))
+                lavadora = Lavadora(id_lavadora, marca_lavadora, modelo_lavadora, prec_lavadora, capacidad_carga, consumo_agua, ciclos_lavado)     
+                      
+        
+            elif opcion_insta == 2:
+                id_refrigerador = input("ingrese la id del refrigerador")
+                marca_refrigerador = input("ingrese la marca del refrigerador")
+                modelo_refrigerador = input("ingrese el modelo del refrigerador")
+                prec_refrigerador = float(input("ingrese el precio del refrigerador"))
+                no_puertas = int(input("ingrese el numero de puertas del refrigerador"))
+                metros_cubicos = int(input("ingrese los metros cubicos del refrigerador"))
+                pies_capacidad = int(input("ingrese la capacidad en pies cubicos del refrigerador"))
+                refrigerador = Refrigerador(id_refrigerador, marca_refrigerador, modelo_refrigerador, prec_refrigerador, no_puertas, metros_cubicos, pies_capacidad)
+        
+            elif opcion_insta == 3:
+                id_microondas = input("ingrese la id del microondas")
+                marca_microondas = input("ingrese la marca del microondas")
+                modelo_microondas = input("ingrese el modelo del microondas")
+                prec_microondas = float(input("ingrese el precio del microondas"))
+                potencia = int(input("ingrese la potencia del microondas en W"))
+                consumo_energia = int(input("ingrese el consumo de energia del microondas en W"))
+                medidas = input("ingrese las medidas del microondas (Ancho, Alto, Profundiad)")
+                microondas = Microondas(id_microondas, marca_microondas, modelo_microondas, prec_microondas, potencia, consumo_energia, medidas)   
+            else:
+        
+                raise ValueError("Error inválido por favor ingrese un número del 1 al 3") 
+        
+                 
+                #lavadora = Lavadora("8MWTW2224WJM", "Whirlpool", "8MWTW2224WJM", 13999, 22, 15 ,12)
+                #refrigerador = Refrigerador("RF22A4010S9/EM","Samsung","RF22A4010S9/EM",30000,3,0.623,22)
+                #microondas = Microondas ("MH1596DIR","LG","NeoChef",4700,1200,1350,"54x32.2x43.3")
         elif opcion == 2:
             print(f"\n\n{lavadora}")
             print(f"\n\n{refrigerador}")
@@ -100,14 +137,7 @@ while True:
         print("Error: Debe ingresar un número entero.")
         print("Detalle del error:", error_valor)
         print("Intente de nuevo.\n")
-    
-    except NameError as error_nombre:
-        print("Error: Intentaste acceder a un electrodoméstico que aún no ha sido creado.")
-        print("Detalle del error:", error_nombre)
-        print("Intente de nuevo.\n")
-        
-    except KeyboardInterrupt as interrupcion:
-        print("\nPrograma interrumpido por el usuario.")
+  
     
     except Exception as error_general:
         print("Ha ocurrido un error inesperado.")
